6fff8d77b40108a55806c5edf1b8a719c3b1ddfc
bernadettechrestella <bernadettechrestella@gmail.com>
2021-12-14 10:34:22 +0900

    add button send message

--- src/components/contactUs/FormPartnership.vue
+++ src/components/contactUs/FormPartnership.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-container>
+    <v-container class="pb-12 mb-6">
         <p class="text-center font-weight-bold text-h3 pt-5 pb-5">Form Partnership</p>
         <p class="text-center pb-5">Tertarik menjalin kerjasama dengan kami untuk mencapai peningkatan literasi anak di Indonesia? Anda dapat mengisi form di bawah ini, Tim kami akan menghubungi Anda untuk proses selanjutnya.</p>
         <v-text-field
@@ -23,6 +23,13 @@
           color="#FF5E3C"
           append-icon="mdi-square-edit-outline"
         ></v-textarea>
+        <v-btn
+            rounded
+            color="#FF5E3C"
+            class="white--text"
+            width="300">
+            Kirim Pesan
+        </v-btn>
     </v-container>
 </template>
 
