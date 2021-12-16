33a0d9ad4ebeac9c2c20b7619e5044e17216a0e3
Afra Rian <42569393+bernadettechrestella@users.noreply.github.com>
2021-12-16 11:57:41 +0700

    Update Responsive Mobile

--- src/components/mainView/Opening.vue
+++ src/components/mainView/Opening.vue
@@ -1,19 +1,37 @@
 <template>
     <v-container>
             <v-card flat class="text-center pb-5">
-                <v-row class="align-center">
-                <v-col cols="5">
-                    <p class="font-weight-bold text-h3">Kami percaya bahwa literasi adalah kunci bagi anak-anak untuk mengakses dunia</p>
+                <v-row class="align-center" justify="center" justify-sm="center">
+                <v-col v-if="$vuetify.breakpoint.smAndDown" lg="5" sm="5">
+                    <v-img
+                        src= "~@/assets/gambarABC.svg"
+                        width="100%"
+                        :min-height="initHeight"
+                        contain
+                    ></v-img>
+                </v-col>
+                <v-col lg="5" sm="5">
+                    <p v-if="$vuetify.breakpoint.mdAndUp" class="font-weight-bold text-h3">Kami percaya bahwa literasi adalah kunci bagi anak-anak untuk mengakses dunia</p>
+                    <p v-else class="font-weight-bold text-h5">Kami percaya bahwa literasi adalah kunci bagi anak-anak untuk mengakses dunia</p>
                     &emsp;
                     <v-btn
+                        v-if="$vuetify.breakpoint.mdAndUp"
                         rounded
                         color="#FF5E3C"
                         class="white--text text-h6"
                         height="50">
                         Download Aplikasi
                     </v-btn>
+                    <v-btn
+                        v-else
+                        rounded
+                        color="#FF5E3C"
+                        class="white--text text-h8"
+                        height="20">
+                        Download Aplikasi
+                    </v-btn>
                 </v-col>
-                <v-col cols="7">
+                <v-col v-if="$vuetify.breakpoint.mdAndUp" lg="7" sm="7">
                     <v-img
                         src= "~@/assets/gambarABC.svg"
                         width="100%"
