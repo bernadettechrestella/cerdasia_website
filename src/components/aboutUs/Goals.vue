f89456f9fc611d4c49f8b9e79902146748bab3e3
bernadettechrestella <bernadettechrestella@gmail.com>
2021-12-16 23:32:17 +0900

    fix goal and testimoni

--- src/components/aboutUs/Goals.vue
+++ src/components/aboutUs/Goals.vue
@@ -35,36 +35,40 @@
 
         <v-img v-if="$vuetify.breakpoint.smAndDown"
             src="~@/assets/kuningputih.png"
-            height="420">
+            height="300">
             <v-container>
-            <v-row class="pt-5">
                 <v-col>
-                    <v-card :class="`rounded${'-xl'}`" class="text-center pa-5" height="370">
+                    <v-card :class="`rounded${'-xl'}`" class="text-center pa-5">
                         <p class="text-h4 font-weight-bold">SDG 1</p>
                         <p>No Poverty</p>
                         <p>Kami menekankan pada pemerataan kualitas edukasi anak sehingga mereka memiliki kesempatan yang sama untuk memperjuangkan masa depan mereka</p>
                     </v-card>
                 </v-col>
-                &emsp;
+            </v-container>
+        </v-img>
+
+
+        <v-img v-if="$vuetify.breakpoint.smAndDown"
+            src="~@/assets/kuningputih.png"
+            height="300">
+            <v-container>
                 <v-col>
-                    <v-card :class="`rounded${'-xl'}`" class="text-center pa-5" height="370">
+                    <v-card :class="`rounded${'-xl'}`" class="text-center pa-5">
                         <p class="text-h4 font-weight-bold">SDG 4</p>
                         <p>Quality Education</p>
                         <p>Berfokus pada kesetaraan kualitas pendidikan dimana semua anak di Indonesia memiliki hak untuk dapat mengembangkan potensi dirinya melalui peningkatan kualitas edukasi</p>
                     </v-card>
                 </v-col>
-            </v-row>
             </v-container>
         </v-img>
-
         
         <v-img v-if="$vuetify.breakpoint.smAndDown"
             src="~@/assets/kuningputih.png"
-            height="270">
+            height="330">
             <v-container>
             <v-row class="pt-5">
                 <v-col>
-                    <v-card :class="`rounded${'-xl'}`" class="text-center pa-5" height="230">
+                    <v-card :class="`rounded${'-xl'}`" class="text-center pa-5">
                         <p class="text-h4 font-weight-bold">SDG 8</p>
                         <p>Decent Work & Economic Growth</p>
                         <p>Berusaha menanamkan sedini mungkin kemampuan menganalisis informasi, daya berpikir kritis, dan kemerdekaan memiliki pendapat pada anak yang nantinya dapat berguna dalam dunia kerja</p>
--- src/components/mainView/Testimoni.vue
+++ src/components/mainView/Testimoni.vue
@@ -3,92 +3,104 @@
         <div class="pt-5 mt-5 pb-5 mb-5" v-if="$vuetify.breakpoint.mdAndUp">
             <p class="font-weight-bold text-h3 pb-5">Testimoni</p>
             <v-row>
-                <v-card flat>
-                    <v-img
+                <v-col>
+                <v-card color="#FF5E3C" :class="`rounded${'-xl'}`">
+                    <!-- <v-img
                         height="380"
-                        src="~@/assets/card.svg">
+                        src="~@/assets/card.svg"> -->
                         <p class="white--text pt-5 text-h6 text-center">Incredible Experience</p>
                         <p class="white--text pl-3 pr-3 text-center">Dependable, Responsive, Professional Partner We had an incredible experience working with Landify and were impressed they made such a big difference in only three weeks. Our team is so grateful for the wonderful improvements they made and their ability to get familiar with the concept so quickly. It acted as a catalyst to take our design to the next level and get more eyes on our product. Fermin Apps has collaborated with Landify team for several projects such as Photo Sharing Apps and Custom Social Networking Apps. The experience has been pleasant, professional and exceeding our expectations. The team is always thinking beyond the current tasks and helps us formulate a vision and possibilities for future.</p>
-                    </v-img>
-                    <v-row>
+                    <!-- </v-img> -->
+                    <v-divider />
+                    <v-row class="pt-2">
                         <v-col class="text-end pt-5">
-                            <v-avatar color="red">
-                                <span class="white--text text-h5">A</span>
+                            <v-avatar color="yellow">
+                                <span class="text-h5 text-center">A</span>
                             </v-avatar>
                         </v-col>
                         <v-col>
-                            <p>Jane Doe</p>
-                            <p>CEO, Cerdasia</p>
+                                <p class="white--text">Jane Doe</p>
+                                <p class="white--text">CEO, Cerdasia</p>
                         </v-col>
                     </v-row>
                 </v-card>
+                </v-col>
                 &emsp;
-                <v-card flat>
-                    <v-img
+                <v-col>
+                <v-card color="#FF5E3C" :class="`rounded${'-xl'}`">
+                    <!-- <v-img
                         height="380"
-                        src="~@/assets/card.svg">
+                        src="~@/assets/card.svg"> -->
                         <p class="white--text pt-5 text-h6 text-center">Incredible Experience</p>
-                        <p class="white--text pl-3 pr-3">Dependable, Responsive, Professional Partner We had an incredible experience working with Landify and were impressed they made such a big difference in only three weeks. Our team is so grateful for the wonderful improvements they made and their ability to get familiar with the concept so quickly. It acted as a catalyst to take our design to the next level and get more eyes on our product. Fermin Apps has collaborated with Landify team for several projects such as Photo Sharing Apps and Custom Social Networking Apps. The experience has been pleasant, professional and exceeding our expectations. The team is always thinking beyond the current tasks and helps us formulate a vision and possibilities for future.</p>
-                    </v-img>
-                    <v-row>
+                        <p class="white--text pl-3 pr-3 text-center">Dependable, Responsive, Professional Partner We had an incredible experience working with Landify and were impressed they made such a big difference in only three weeks. Our team is so grateful for the wonderful improvements they made and their ability to get familiar with the concept so quickly. It acted as a catalyst to take our design to the next level and get more eyes on our product. Fermin Apps has collaborated with Landify team for several projects such as Photo Sharing Apps and Custom Social Networking Apps. The experience has been pleasant, professional and exceeding our expectations. The team is always thinking beyond the current tasks and helps us formulate a vision and possibilities for future.</p>
+                    <!-- </v-img> -->
+                    <v-divider />
+                    <v-row class="pt-2">
                         <v-col class="text-end pt-5">
-                            <v-avatar color="red">
-                                <span class="white--text text-h5 text-center">A</span>
+                            <v-avatar color="yellow">
+                                <span class="text-h5 text-center">A</span>
                             </v-avatar>
                         </v-col>
                         <v-col>
-                                <p>Jane Doe</p>
-                                <p>CEO, Cerdasia</p>
+                                <p class="white--text">Jane Doe</p>
+                                <p class="white--text">CEO, Cerdasia</p>
                         </v-col>
                     </v-row>
                 </v-card>
+                </v-col>
             </v-row>
         </div>
 
 
         <div class="pt-5 mt-5 pb-5 mb-5" v-if="$vuetify.breakpoint.smAndDown">
-            <p class="font-weight-bold text-h3">Testimoni</p>
-            <v-row>
-                <v-card flat class="pl-5 pr-5 pt-2">
-                    <v-img
-                        height="520"
-                        src="~@/assets/card.svg">
+            <p class="font-weight-bold text-h3 pb-5">Testimoni</p>
+            <!-- <v-row> -->
+                <v-col>
+                <v-card color="#FF5E3C" :class="`rounded${'-xl'}`">
+                    <!-- <v-img
+                        height="380"
+                        src="~@/assets/card.svg"> -->
                         <p class="white--text pt-5 text-h6 text-center">Incredible Experience</p>
                         <p class="white--text pl-3 pr-3 text-center">Dependable, Responsive, Professional Partner We had an incredible experience working with Landify and were impressed they made such a big difference in only three weeks. Our team is so grateful for the wonderful improvements they made and their ability to get familiar with the concept so quickly. It acted as a catalyst to take our design to the next level and get more eyes on our product. Fermin Apps has collaborated with Landify team for several projects such as Photo Sharing Apps and Custom Social Networking Apps. The experience has been pleasant, professional and exceeding our expectations. The team is always thinking beyond the current tasks and helps us formulate a vision and possibilities for future.</p>
-                    </v-img>
-                    <v-row>
+                    <!-- </v-img> -->
+                    <v-divider />
+                    <v-row class="pt-2">
                         <v-col class="text-end pt-5">
-                            <v-avatar color="red">
-                                <span class="white--text text-h5">A</span>
+                            <v-avatar color="yellow">
+                                <span class="text-h5 text-center">A</span>
                             </v-avatar>
                         </v-col>
                         <v-col>
-                            <p>Jane Doe</p>
-                            <p>CEO, Cerdasia</p>
+                                <p class="white--text">Jane Doe</p>
+                                <p class="white--text">CEO, Cerdasia</p>
                         </v-col>
                     </v-row>
                 </v-card>
+                </v-col>
                 &emsp;
-                <v-card flat class="pl-5 pr-5 pb-5">
-                    <v-img
-                        height="520"
-                        src="~@/assets/card.svg">
+                <v-col>
+                <v-card color="#FF5E3C" :class="`rounded${'-xl'}`">
+                    <!-- <v-img
+                        height="380"
+                        src="~@/assets/card.svg"> -->
                         <p class="white--text pt-5 text-h6 text-center">Incredible Experience</p>
                         <p class="white--text pl-3 pr-3 text-center">Dependable, Responsive, Professional Partner We had an incredible experience working with Landify and were impressed they made such a big difference in only three weeks. Our team is so grateful for the wonderful improvements they made and their ability to get familiar with the concept so quickly. It acted as a catalyst to take our design to the next level and get more eyes on our product. Fermin Apps has collaborated with Landify team for several projects such as Photo Sharing Apps and Custom Social Networking Apps. The experience has been pleasant, professional and exceeding our expectations. The team is always thinking beyond the current tasks and helps us formulate a vision and possibilities for future.</p>
-                    </v-img>
-                    <v-row>
+                    <!-- </v-img> -->
+                    <v-divider />
+                    <v-row class="pt-2">
                         <v-col class="text-end pt-5">
-                            <v-avatar color="red">
-                                <span class="white--text text-h5 text-center">A</span>
+                            <v-avatar color="yellow">
+                                <span class="text-h5 text-center">A</span>
                             </v-avatar>
                         </v-col>
                         <v-col>
-                                <p>Jane Doe</p>
-                                <p>CEO, Cerdasia</p>
+                                <p class="white--text">Jane Doe</p>
+                                <p class="white--text">CEO, Cerdasia</p>
                         </v-col>
                     </v-row>
                 </v-card>
-            </v-row>
+                </v-col>
+            <!-- </v-row> -->
         </div>
 
     </v-container>
