10eaa81125854d2bd2d1d19f374a6951f5157561
bernadettechrestella <bernadettechrestella@gmail.com>
2021-12-16 22:29:48 +0900

    Lingkaran kuning

--- src/components/blog/BlogView.vue
+++ src/components/blog/BlogView.vue
@@ -1,7 +1,7 @@
 <template>
     <v-container>
         <p class="text-h3 font-weight-bold text-center pt-5 pb-5">Blog</p>
-        <v-row class="justify-center pt-5 pb-5">
+        <v-row class="justify-center pt-5 pb-5" v-if="$vuetify.breakpoint.mdAndUp">
             <v-card elevation="5">
                 <v-img
                     height="250"
@@ -20,7 +20,48 @@
                 <v-card-subtitle>Edukasi</v-card-subtitle>
             </v-card>
         </v-row>
-        <v-row class="justify-center pt-5 pb-10 mb-5">
+        <v-row class="justify-center pt-5 pb-10 mb-5" v-if="$vuetify.breakpoint.mdAndUp">
+            <v-card elevation="5">
+                <v-img
+                    height="250"
+                    width="500"
+                    src="~@/assets/gambarABC.svg"></v-img>
+                <v-card-title>Cara Mengajar Anak SD</v-card-title>
+                <v-card-subtitle>Edukasi</v-card-subtitle>
+            </v-card>
+            &emsp;
+            <v-card elevation="5">
+                <v-img
+                    height="250"
+                    width="500"
+                    src="~@/assets/gambarABC.svg"></v-img>
+                <v-card-title>Cara Mengajar Anak SD</v-card-title>
+                <v-card-subtitle>Edukasi</v-card-subtitle>
+            </v-card>
+        </v-row>
+
+
+        <v-row class="justify-center pa-5" v-if="$vuetify.breakpoint.smAndDown">
+            <v-card elevation="5">
+                <v-img
+                    height="250"
+                    width="500"
+                    src="~@/assets/gambarABC.svg"></v-img>
+                <v-card-title>Cara Mengajar Anak SD</v-card-title>
+                <v-card-subtitle>Edukasi</v-card-subtitle>
+            </v-card>
+            &emsp;
+            <v-card elevation="5">
+                <v-img
+                    height="250"
+                    width="500"
+                    src="~@/assets/gambarABC.svg"></v-img>
+                <v-card-title>Cara Mengajar Anak SD</v-card-title>
+                <v-card-subtitle>Edukasi</v-card-subtitle>
+            </v-card>
+        </v-row>
+
+        <v-row class="justify-center pl-5 pr-5 pb-12" v-if="$vuetify.breakpoint.smAndDown">
             <v-card elevation="5">
                 <v-img
                     height="250"
