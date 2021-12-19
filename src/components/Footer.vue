7145a148666baf32e90d1b349d0f61f558886302
bernadettechrestella <bernadettechrestella@gmail.com>
2021-12-19 20:26:46 +0900

    Repair Footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,11 @@
 <template>
+    <div>
+        <v-card flat tile v-if="$vuetify.breakpoint.mdAndUp">
     <v-footer padless>
-        <v-card flat tile width="100%" v-if="$vuetify.breakpoint.mdAndUp">
+        <v-card flat tile width="100%">
             <v-img
                 class="white--text"
-                height="auto"
+                height="260"
                 src="../assets/footer.svg"
                 >
             <v-row class="pa-5">
@@ -22,8 +24,8 @@
                     </v-card-text>
                 </v-col>
                 <v-col>
-                    <v-card-text>
-                        <p text class="text-h7">//alamat</p>
+                    <v-card-text class="pt-8">
+                        <!-- <p text class="text-h7">//alamat</p> -->
                         <v-row align="center">
                                 <v-icon 
                                 dark
@@ -33,9 +35,9 @@
                                     mdi-email
                                 </v-icon>
                             &emsp;
-                                <p text class="text-h7 pt-4">ingfo@blabla.com</p>
+                            <a class="text-h7 white--text" href="mailto:%20cerdasiateam@gmail.com">cerdasiateam@gmail.com</a>
                         </v-row>
-                        <v-row align="center">
+                        <v-row align="center" class="pt-5">
                                 <v-icon 
                                 dark
                                 fab
@@ -44,7 +46,7 @@
                                     mdi-phone
                                 </v-icon>
                             &emsp;
-                                <p text class="text-h7 pt-4">+62 xxx xxxx xxxx</p>
+                            <a text class="text-h7 white--text" href="https://wa.me/08998067422">+62 8998067422</a>
                         </v-row>
                     </v-card-text>
                 </v-col>
@@ -82,90 +84,89 @@
             </v-card-text>
             </v-img>
         </v-card>
+    </v-footer>
+        </v-card>
 
 
         <v-card flat tile color="#FDD835" width="100%" v-else>
-            <v-img
-                class="white--text align-end"
-                height="auto"
-                src="../assets/footer.svg"
-                >
-            <v-row class="mt-2">
-                <v-col sm="6">
-                    <v-card-text>
-                        <p text class="text-h5">Cerdasia</p>
-                        <!-- <p text class="text-h7">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem</p>
-                        <p text class="text-h7">cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua</p> -->
-                    </v-card-text>
-                </v-col>
-                <v-col>
-                    <v-card-text>
-                        <p text class="text-h6">Bantuan & Panduan</p>
-                        <p text class="text-h7" @click="redirectKebijakanPrivasi()">Kebijakan Privasi</p>
-                    </v-card-text>
-                </v-col>
-                <v-col>
-                    <v-card-text>
-                        <p text class="text-h7">//alamat</p>
-                        <v-row align="center">
-                                <v-icon 
-                                dark
-                                fab
-                                small
-                                color="white">
-                                    mdi-email
-                                </v-icon>
-                            &emsp;
-                                <p text class="text-h7 pt-4">ingfo@blabla.com</p>
-                        </v-row>
-                        <v-row align="center">
-                                <v-icon 
-                                dark
-                                fab
-                                small
-                                color="white">
-                                    mdi-phone
-                                </v-icon>
-                            &emsp;
-                                <p text class="text-h7 pt-4">+62 xxx xxxx xxxx</p>
-                        </v-row>
-                    </v-card-text>
-                </v-col>
-            </v-row>
-            <v-row align="center" justify="center">
-                            <p text class="text-h7 pt-4">Connect with us</p>
-                            &emsp;
-                            <v-icon 
-                                dark
-                                fab
-                                small
-                                color="white">
-                                    mdi-facebook
-                                </v-icon>
-                            &emsp;
-                            <v-icon 
-                                dark
-                                fab
-                                small
-                                color="white">
-                                    mdi-linkedin
-                                </v-icon>
-                            &emsp;
-                            <v-icon 
-                                dark
-                                fab
-                                small
-                                color="white">
-                                    mdi-instagram
-                                </v-icon>
-                        </v-row>
+            <v-footer padless>
+                <v-card flat tile>
+                    <v-img
+                        class="white--text align-end"
+                        height="auto"
+                        src="../assets/footer.svg"
+                        >
+                    <v-row class="mt-2">
+                        <v-col sm="6">
+                            <v-card-text>
+                                <p text class="text-h5">Cerdasia</p>
+                            </v-card-text>
+                        </v-col>
+                        <v-col>
+                            <v-card-text>
+                                <p text class="text-h6">Bantuan & Panduan</p>
+                                <p text class="text-h7" @click="redirectKebijakanPrivasi()">Kebijakan Privasi</p>
+                            </v-card-text>
+                        </v-col>
+                    </v-row>
+                    <v-row class="pl-7">
+                        <v-icon 
+                            dark
+                            fab
+                            small
+                            color="white">
+                            mdi-email
+                        </v-icon>
+                        &emsp;
+                        <a text class="text-h7 white--text" href="mailto:cerdasiateam@gmail.com">cerdasiateam@gmail.com</a>
+                    </v-row>
+                    <v-row class="pl-7 pt-3">
+                        <v-icon 
+                            dark
+                            fab
+                            small
+                            color="white">
+                            mdi-phone
+                        </v-icon>
+                        &emsp;
+                        <a text class="text-h7 white--text" href="https://wa.me/08998067422">+62 8998067422</a>
+                    </v-row>
+                    <v-row align="center" justify="center" class="pt-3">
+                        <p text class="text-h7 pt-4">Connect with us</p>
+                        &emsp;
+                        <v-icon 
+                            dark
+                            fab
+                            small
+                            color="white">
+                            mdi-facebook
+                        </v-icon>
+                        &emsp;
+                        <v-icon 
+                            dark
+                            fab
+                            small
+                            color="white">
+                            mdi-linkedin
+                        </v-icon>
+                        &emsp;
+                        <v-icon 
+                            dark
+                            fab
+                            small
+                            color="white">
+                            mdi-instagram
+                        </v-icon>
+                    </v-row>
             <!-- <v-divider /> -->
             <v-card-text flat tile width="100%" class="white--text text-right">
                 <strong>Copyright © 2021 Cerdasia. All rights reserved. Powered by YLabs</strong>
             </v-card-text>
             </v-img>
+                </v-card>
+        </v-footer>
         </v-card>
-    </v-footer>
+            </div>
 </template>
 
 <script>
